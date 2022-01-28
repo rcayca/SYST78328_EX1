58a82d1a2c0dc67cc946b2394e7ffcf81b56fe97
PatelNandini <panandin@sheridancollege.ca>
2022-01-28 01:22:00 -0500

    Second commit.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -13,7 +13,9 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye
+ * Date January 27,2022
  * @modified by Nandini Patel
+ * Student ID: 991622118
  * 
  */
 public class Card {
