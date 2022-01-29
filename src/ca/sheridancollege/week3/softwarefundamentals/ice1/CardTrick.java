6d3d2ead3b86ec13e6d132f79e1ef9b599f59d8a
BhattAbh <98198502+BhattAbh@users.noreply.github.com>
2022-01-29 18:06:22 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -23,22 +23,22 @@ public static void main(String[] args) {
            Scanner input = new Scanner(System.in);
 
         //Create an array to hold 7 cards
-        Card[] hand = new Card[7];
+        Card[] magicHand = new Card[7];
 
     
         Random random = new Random();
 
-       for (int i = 0; i < hand.length; i++) {
+       for (int i = 0; i < magicHand.length; i++) {
             int value = random.nextInt(13) + 1;
             String suit = Card.SUITS[random.nextInt(4)];
 
             Card card = new Card(value, suit);
-            hand[i] = card;
+            magicHand[i] = card;
         }
 
        
         System.out.println("Here are the cards in the hand");
-        for (Card card : hand) {
+        for (Card card : magicHand) {
             System.out.printf("%d of %s\n", card.getValue(), card.getSuit());
         }
 
@@ -55,7 +55,7 @@ public static void main(String[] args) {
         Card userGuess = new Card(value, Card.SUITS[suit - 1]);
 
         boolean match = false;
-        for (Card card : hand) {
+        for (Card card : magicHand) {
             if (card.getValue() == userGuess.getValue()
                     && card.getSuit().equals(userGuess.getSuit())) {
                 match = true;
