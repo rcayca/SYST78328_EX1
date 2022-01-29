b32684235699abd9099a92425fd85f3ad0f0bccb
starnvir <sitarnvi@sheridancollege.ca>
2022-01-29 16:16:34 -0500

    Tarnvir-Singh-Ex1 branch added

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,16 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
- * @modified by Megha Patel
+ * Date - 29 January 2022
+ * Student ID number - 991638279
+ * @author Tarnvir Singh
+ * @modified by Tarnvir Singh
  * 
  */
 public class CardTrick {
@@ -22,8 +22,39 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-             }
+            
+            c.setSuit(c.SUITS[(int)(Math.random()*4)]);
+            c.setValue((int)(1 + (Math.random()*13)));
+            magicHand[i] = c;
+            
+        }
+        Scanner input = new Scanner(System.in);
+        Card c= new Card();
         
-         }
+        System.out.print("Enter card number:");
+        c.setValue(input.nextInt());
+        
+        System.out.print("Enter the suit; Hearts, Diamonds, Spades, Clubs : ");
+        c.setSuit(input.next());
+        
+        boolean card = false;
+        for(int i=0; i < magicHand.length; i++){
+            if(magicHand[i].getValue()==c.getValue() && magicHand[i].getSuit().equalsIgnoreCase(c.getSuit())){
+                card = true;
+                break;
+            }else{
+                card = false;
+            }
+        }
+        if(card){
+            System.out.println("Card is inside stack.");
+        }else{
+            System.out.println("Card is not in Stack.");
+        }
+    }
+    
+    public static void printInfo(){
+        
+    }
     
 }
