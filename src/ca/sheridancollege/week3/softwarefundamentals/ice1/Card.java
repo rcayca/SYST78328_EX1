4472cc70e88576e1193b230fb161e418b7640b80
ankit <kukretia@sheridancollege.ca>
2022-01-29 19:17:48 -0500

    added name and student id in the header manually

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -6,14 +6,8 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author dancye
- * @modified by Megha Patel
+ * Ankit Kukreti
+ * Student id: 991615176
  * 
  */
 public class Card {
